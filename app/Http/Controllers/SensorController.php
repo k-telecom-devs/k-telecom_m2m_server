ef2ab5a16964aa8bed695f23827fe8485e0f24d2
Alexandr <kom.alx.frlv@gmail.com>
2022-10-27 16:36:17 +0500

    eblan22814882281488

--- app/Http/Controllers/SensorController.php
+++ app/Http/Controllers/SensorController.php
@@ -51,10 +51,7 @@ class SensorController extends Controller
             $real_device_type = DeviceType::find($request->device_type_id);
 
             return response()->json([
-                'message' => 'Wrong sensor type. this version only for '
-                    .$version_device_type
-                    .". Your device is "
-                    . $real_device_type]);
+                'message' => ''.$version]);
 
            if($version->device_type_id == $request->device_type_id){
                 $sensor_settings->version_id = $request->version_id;
